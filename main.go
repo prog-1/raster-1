37a1b239463a2757069184b4b3b5c91334985b8e
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2023-02-26 01:47:08 +0200

    without rotation

--- main.go
+++ main.go
@@ -20,42 +20,41 @@ type Point struct {
 //Implement a program that rotates a vector. The zero point must be in the middle of the window. You must not use any built-in rotation functionality. Just animate an angle and recompute point position using cos/sin functions
 
 func DrawLineDDA(screen *ebiten.Image, p0, p1 Point, color color.Color) {
-	dx := math.Abs(p1.x - p0.x)
-	dy := math.Abs(p1.y - p0.y)
-	if p1.x < p0.x {
-		p0, p1 = p1, p0
-	}
-	if dx >= dy {
-		k := dy / dx
-		for x, y := p0.x, p0.y+0.5; x <= p1.x; x, y = x+1, y+k {
+	if math.Abs(p1.x-p0.x) >= math.Abs(p1.y-p0.y) {
+		if p0.x > p1.x {
+			p0, p1 = p1, p0
+		}
+		y := p0.y
+		for x := p0.x; x <= p1.x; x++ {
 			screen.Set(int(x), int(y), color)
+			y += (p1.y - p0.y) / (p1.x - p0.x)
 		}
 	} else {
-		k := dx / dy
-		for x, y := p0.x+0.5, p0.y; y <= p1.y; x, y = x+k, y+1 {
+		if p0.y > p1.y {
+			p0, p1 = p1, p0
+		}
+		x := p0.x
+		for y := p0.y; y <= p1.y; y++ {
 			screen.Set(int(x), int(y), color)
+			x += (p1.x - p0.x) / (p1.y - p0.y)
 		}
 	}
-
 }
 
 type game struct {
-	angle  float64
-	p0, p1 Point
 }
 
 func (g *game) Layout(outWidth, outHeight int) (w, h int) { return screenWidth, screenHeight }
 func (g *game) Update() error {
-	g.angle += math.Pi / 180
+
 	return nil
 }
 
 func (g *game) Draw(screen *ebiten.Image) {
-	g.p0.x = screenWidth / 2
-	g.p0.y = screenHeight / 2
-	g.p1.x = (math.Cos(g.angle) * (-math.Sin(g.angle))) * g.p0.x
-	g.p1.y = math.Sin(g.angle) * math.Cos(g.angle) * g.p0.y
-	DrawLineDDA(screen, g.p0, g.p1, color.RGBA{255, 0, 0, 255})
+	DrawLineDDA(screen, Point{0, 0}, Point{screenWidth, screenHeight}, color.RGBA{255, 0, 0, 255})
+	DrawLineDDA(screen, Point{screenWidth, 0}, Point{0, screenHeight}, color.RGBA{255, 0, 0, 255})
+	DrawLineDDA(screen, Point{screenWidth / 2, 0}, Point{screenWidth / 2, screenHeight}, color.RGBA{255, 0, 0, 255})
+	DrawLineDDA(screen, Point{0, screenHeight / 2}, Point{screenWidth, screenHeight / 2}, color.RGBA{255, 0, 0, 255})
 }
 
 func main() {
