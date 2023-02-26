e042af29dc342cbd2c786f1927353ea7686ac9b8
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2023-02-26 02:22:10 +0200

    rotation done, a little bit weird though

--- main.go
+++ main.go
@@ -42,19 +42,31 @@ func DrawLineDDA(screen *ebiten.Image, p0, p1 Point, color color.Color) {
 }
 
 type game struct {
+	angle  float64
+	p0, p1 Point
+}
+
+func RotateLine(p0, p1 Point, angle float64) Point {
+	p1.x = ((p1.x-p0.x)*math.Cos(angle) - (p1.y-p0.y)*math.Sin(angle)) + p0.x
+	p1.y = ((p1.x-p0.x)*math.Sin(angle) + (p1.y-p0.y)*math.Cos(angle)) + p0.y
+	return p1
 }
 
 func (g *game) Layout(outWidth, outHeight int) (w, h int) { return screenWidth, screenHeight }
 func (g *game) Update() error {
-
+	g.angle += math.Pi / 180
 	return nil
 }
 
 func (g *game) Draw(screen *ebiten.Image) {
-	DrawLineDDA(screen, Point{0, 0}, Point{screenWidth, screenHeight}, color.RGBA{255, 0, 0, 255})
-	DrawLineDDA(screen, Point{screenWidth, 0}, Point{0, screenHeight}, color.RGBA{255, 0, 0, 255})
-	DrawLineDDA(screen, Point{screenWidth / 2, 0}, Point{screenWidth / 2, screenHeight}, color.RGBA{255, 0, 0, 255})
-	DrawLineDDA(screen, Point{0, screenHeight / 2}, Point{screenWidth, screenHeight / 2}, color.RGBA{255, 0, 0, 255})
+	g.p0 = Point{screenWidth / 2, screenHeight / 2}
+	g.p1 = Point{screenWidth / 2, screenHeight}
+	g.p1 = RotateLine(g.p0, g.p1, g.angle)
+	DrawLineDDA(screen, g.p0, g.p1, color.RGBA{255, 0, 0, 255})
+	//DrawLineDDA(screen, Point{0, 0}, Point{screenWidth, screenHeight}, color.RGBA{255, 0, 0, 255})
+	///DrawLineDDA(screen, Point{screenWidth, 0}, Point{0, screenHeight}, color.RGBA{255, 0, 0, 255})
+	//DrawLineDDA(screen, Point{screenWidth / 2, 0}, Point{screenWidth / 2, screenHeight}, color.RGBA{255, 0, 0, 255})
+	//DrawLineDDA(screen, Point{0, screenHeight / 2}, Point{screenWidth, screenHeight / 2}, color.RGBA{255, 0, 0, 255})
 }
 
 func main() {
