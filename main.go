7c5ca2c601a6eefe5bd2a1aa244c61e39807648b
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2023-02-26 02:58:55 +0200

    Fully Done! Its 3AM already!

--- main.go
+++ main.go
@@ -19,24 +19,26 @@ type Point struct {
 
 //Implement a program that rotates a vector. The zero point must be in the middle of the window. You must not use any built-in rotation functionality. Just animate an angle and recompute point position using cos/sin functions
 
-func DrawLineDDA(screen *ebiten.Image, p0, p1 Point, color color.Color) {
-	if math.Abs(p1.x-p0.x) >= math.Abs(p1.y-p0.y) {
-		if p0.x > p1.x {
-			p0, p1 = p1, p0
+func DrawLineDDA(screen *ebiten.Image, p0x, p0y, p1x, p1y float64, color color.Color) {
+	if math.Abs(p1x-p0x) >= math.Abs(p1y-p0y) {
+		if p0x > p1x {
+			p0x, p1x = p1x, p0x
+			p0y, p1y = p1y, p0y
 		}
-		y := p0.y
-		for x := p0.x; x <= p1.x; x++ {
+		y := p0y
+		for x := p0x; x <= p1x; x++ {
 			screen.Set(int(x), int(y), color)
-			y += (p1.y - p0.y) / (p1.x - p0.x)
+			y += (p1y - p0y) / (p1x - p0x)
 		}
 	} else {
-		if p0.y > p1.y {
-			p0, p1 = p1, p0
+		if p0y > p1y {
+			p0x, p1x = p1x, p0x
+			p0y, p1y = p1y, p0y
 		}
-		x := p0.x
-		for y := p0.y; y <= p1.y; y++ {
+		x := p0x
+		for y := p0y; y <= p1y; y++ {
 			screen.Set(int(x), int(y), color)
-			x += (p1.x - p0.x) / (p1.y - p0.y)
+			x += (p1x - p0x) / (p1y - p0y)
 		}
 	}
 }
@@ -47,8 +49,10 @@ type game struct {
 }
 
 func RotateLine(p0, p1 Point, angle float64) Point {
-	p1.x = ((p1.x-p0.x)*math.Cos(angle) - (p1.y-p0.y)*math.Sin(angle)) + p0.x
-	p1.y = ((p1.x-p0.x)*math.Sin(angle) + (p1.y-p0.y)*math.Cos(angle)) + p0.y
+	var c Point
+	c.x = ((p1.x-p0.x)*math.Cos(angle) - (p1.y-p0.y)*math.Sin(angle))
+	c.y = ((p1.x-p0.x)*math.Sin(angle) + (p1.y-p0.y)*math.Cos(angle))
+	p1.x, p1.y = c.x, c.y
 	return p1
 }
 
@@ -60,13 +64,12 @@ func (g *game) Update() error {
 
 func (g *game) Draw(screen *ebiten.Image) {
 	g.p0 = Point{screenWidth / 2, screenHeight / 2}
-	g.p1 = Point{screenWidth / 2, screenHeight}
-	g.p1 = RotateLine(g.p0, g.p1, g.angle)
-	DrawLineDDA(screen, g.p0, g.p1, color.RGBA{255, 0, 0, 255})
-	//DrawLineDDA(screen, Point{0, 0}, Point{screenWidth, screenHeight}, color.RGBA{255, 0, 0, 255})
-	///DrawLineDDA(screen, Point{screenWidth, 0}, Point{0, screenHeight}, color.RGBA{255, 0, 0, 255})
-	//DrawLineDDA(screen, Point{screenWidth / 2, 0}, Point{screenWidth / 2, screenHeight}, color.RGBA{255, 0, 0, 255})
-	//DrawLineDDA(screen, Point{0, screenHeight / 2}, Point{screenWidth, screenHeight / 2}, color.RGBA{255, 0, 0, 255})
+	g.p1 = RotateLine(g.p0, Point{0, 0}, g.angle)
+	DrawLineDDA(screen, 0, 0, screenWidth, screenHeight, color.RGBA{255, 0, 0, 255})
+	DrawLineDDA(screen, screenWidth, 0, 0, screenHeight, color.RGBA{255, 0, 0, 255})
+	DrawLineDDA(screen, screenWidth/2, 0, screenWidth/2, screenHeight, color.RGBA{255, 0, 0, 255})
+	DrawLineDDA(screen, 0, screenHeight/2, screenWidth, screenHeight/2, color.RGBA{255, 0, 0, 255})
+	DrawLineDDA(screen, g.p0.x, g.p0.y, g.p0.x+g.p1.x, g.p0.y+g.p1.y, color.RGBA{0, 255, 0, 255})
 }
 
 func main() {
